72dcaf3c2552a1c92923e23c3934ea22d2c8aa05
Sabarish-7 <83224203+Sabarish-7@users.noreply.github.com>
2021-09-02 10:00:25 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,36 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var playerCount;
+var allPlayers;
+
+
+var form, player, game;
+
+
+
+function preload(){
+   bg=loadImage("game.jpg")
+}
+
+function setup(){
+  canvas = createCanvas(displayWidth - 20, displayHeight-30);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+
+function draw(){
+  if(playerCount === 2){
+    game.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    game.play();
+  }
+  if(gameState === 2){
+    game.end();
+  }
+}
